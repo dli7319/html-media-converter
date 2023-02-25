2601f0a4e0dd7b1e60a99e7ebc0136fa00c00bb5
David Li <david@davidl.me>
2023-02-25 12:04:34 -0500

    Refractor ffmpeg call into a separate function

--- src/MediaConverter.tsx
+++ src/MediaConverter.tsx
@@ -6,6 +6,28 @@ import RenderOutput, { OutputOptions, containerToMime, defaultOutputOptions } fr
 import Log from './Log';
 import styles from './styles/MediaConverter.module.css';
 
+function buildFFmpegCall(file: File, outputOptions: OutputOptions): [string[], string, string] {
+    const container = outputOptions.container || file.name.split('.').pop();
+    if (typeof container !== 'string') {
+        throw new Error('No container selected');
+    }
+    const outputFilename = `output.${container}`;
+    const filters = [];
+    if (outputOptions.container == "gif" && outputOptions.hqGif) {
+        filters.push('split=2[v1][v2];[v1]palettegen=stats_mode=full[palette];[v2][palette]paletteuse=dither=sierra2_4a');
+    }
+    let ffmpegCall = [
+        '-i', file.name,
+        (outputOptions.framerate ? ['-r', outputOptions.framerate.toString()] : []),
+        (outputOptions.pixelFormat ? ['-pix_fmt', outputOptions.pixelFormat] : []),
+        (filters.length > 0 ? ['-filter_complex', '[0]' + filters.join(';')] : []),
+        (outputOptions.webpLoop ? ['-loop', '0'] : []),
+        outputFilename
+    ].flat();
+    const outputMime = containerToMime[container] as string;
+    return [ffmpegCall, outputFilename, outputMime];
+}
+
 export default function MediaConverter() {
 
     const [log, setLog] = useState('');
@@ -50,36 +72,13 @@ export default function MediaConverter() {
         if (selectedFile) {
             setLog('');
             setOutputVideoSrc('');
-            const file = selectedFile;
             await ffmpeg.current.load();
-            const updatedOutputOptions = { ...outputOptions };
-            if (!updatedOutputOptions.container) {
-                updatedOutputOptions.container = file.name.split('.').pop();
-            }
-            if (!updatedOutputOptions.container) {
-                console.warn('No container selected');
-                return;
-            }
-            ffmpeg.current.FS('writeFile', file.name, await fetchFile(file));
-            const outputFilename = `output.${updatedOutputOptions.container}`;
-            const filters = [];
-            if (outputOptions.container == "gif" && outputOptions.hqGif) {
-                filters.push('split=2[v1][v2];[v1]palettegen=stats_mode=full[palette];[v2][palette]paletteuse=dither=sierra2_4a');
-            }
-            let ffmpegCall = [
-                '-i', file.name,
-                (updatedOutputOptions.framerate ? ['-r', updatedOutputOptions.framerate.toString()] : []),
-                (updatedOutputOptions.pixelFormat ? ['-pix_fmt', updatedOutputOptions.pixelFormat] : []),
-                (filters.length > 0 ? ['-filter_complex', '[0]' + filters.join(';')] : []),
-                (updatedOutputOptions.webpLoop ? ['-loop', '0'] : []),
-                outputFilename
-            ].flat();
-            const outputMime = containerToMime[updatedOutputOptions.container] as string;
+            ffmpeg.current.FS('writeFile', selectedFile.name, await fetchFile(selectedFile));
+            const [ffmpegCall, outputFilename, outputMime] = buildFFmpegCall(selectedFile, outputOptions);
             await ffmpeg.current.run(...ffmpegCall);
             const data = ffmpeg.current.FS('readFile', outputFilename);
             const url = URL.createObjectURL(new Blob([data.buffer], { type: outputMime }));
             setOutputVideoSrc(url);
-            setOutputOptions(updatedOutputOptions);
             resetFFmpeg();
         } else {
             console.warn('No file selected');
@@ -89,8 +88,9 @@ export default function MediaConverter() {
     const componentClasses = `text-center ${styles.components}`;
     return <div className={`text-center d-flex flex-wrap ${styles.mainContainer}`}>
         <FileInput className={componentClasses} onChange={transcode} file={selectedFile} />
-        <RenderOutput className={componentClasses} log={log}
-            outputOptions={outputOptions} setOutputOptions={setOutputOptions}
+        <RenderOutput className={componentClasses}
+            outputOptions={outputOptions}
+            setOutputOptions={setOutputOptions}
             outputVideoSrc={outputVideoSrc}
             progress={progress}
             onStartRenderClicked={
